237a7b72b5a8cc3447a598144741b8d8648ea3a6
Michael Tissen <m.tissen@posteo.de>
2024-11-10 15:31:54 +0100

    Schritt 2

---
+++ lib/schaltjahr/is_leap_jahr.dart
@@ -0,0 +1,3 @@
+bool isLeapJahr(int year) {
+  return year % 400 == 0;
+}
--- test/leapyears_test.dart
+++ test/leapyears_test.dart
@@ -1,18 +1,19 @@
 import 'package:checks/checks.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd_flutter/schaltjahr/is_leap_jahr.dart';
 
 /*
- - 2000 ist ein Schaltjahr
- - 1900 ist KEIN Schaltjahr
- - 2008 ist ein Schaltjahr
- - 2017 ist kein Schaltjahr
+ - 2000 ist ein Schaltjahr (X)
+ - 1900 ist KEIN Schaltjahr (X)
+ - 2008 ist ein Schaltjahr ()
+ - 2017 ist kein Schaltjahr ()
 */
 void main() {
   test('TeilbarDurch400IstSchaltjahr', () {
     check(isLeapJahr(2000)).isTrue();
   });
-}
 
-bool isLeapJahr(int i) {
-  throw Exception();
+  test('TeilbarDurch100AberNicht400IstKEINSchaltjahr', () {
+    check(isLeapJahr(1900)).isFalse();
+  });
 }
