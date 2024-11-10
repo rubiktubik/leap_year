1481378c8c0ecee51da467dcec59be63ae513a43
Michael Tissen <m.tissen@posteo.de>
2024-11-10 15:15:18 +0100

    Beispiele als Kommentar

--- test/leapyears_test.dart
+++ test/leapyears_test.dart
@@ -1,6 +1,12 @@
 import 'package:checks/checks.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+/*
+ - 2000 ist ein Schaltjahr
+ - 1900 ist KEIN Schaltjahr
+ - 2008 ist ein Schaltjahr
+ - 2017 ist kein Schaltjahr
+*/
 void main() {
   test('isLeapYear', () {
     check(true).isTrue();
